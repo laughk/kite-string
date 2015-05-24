8d96eb8277dfbd41ec1ad941eca5767a835025ac
Kei Iwasaki <me@laughk.org>
2015-05-24 23:31:48 +0900

    add setup.py for installing

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+from kite.version import get_version
+
+setup(
+
+    name='kite',
+    version=get_version(),
+    packages=find_packages(),
+    include_package_data=True,
+    include_requires=[
+        'requests',
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        kite=kite.cli:main
+    ''',
+)
+
